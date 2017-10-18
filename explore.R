cdba24cb841d8b5da8143c7411460f478d72c4e1
Michael Jetsupphasuk <michaeljets@berkeley.edu>
2017-10-18 08:41:17 -0700

    add data

--- explore.R
+++ explore.R
@@ -2,48 +2,56 @@ setwd("~/Stat_157/induced_seismicity")
 
 library(dplyr)
 library(stringr)
-library(raster)
-library(rgdal)
-library(sp)
+
 library(maps)
 library(maptools)
 
+library(sp)  # vector data
+library(raster)  # raster data
+library(rgdal)  # input/output, projections
+library(rgeos)  # geometry ops
+library(spdep)  # spatial dependence
 
-# dat = data.frame()
-# start_times = paste0(seq(2010, 1970, by = -5), '-01-01')
-# end_times = c('2017-10-14', start_times[1:8])
-# for (i in 1:length(times)){
-#   str1 = "https://earthquake.usgs.gov/fdsnws/event/1/query.csv?starttime="
-#   start = start_times[i]
-#   str2 = "%2000:00:00&endtime="
-#   end = end_times[i]
-#   str3 = "%2023:59:59&maxlatitude=42.183&minlatitude=32.495&maxlongitude=-113.994&minlongitude=-124.805&minmagnitude=2.0&orderby=time"
-#   print(time)
-#   dat = bind_rows(dat, read.csv(url(paste0(str1, start, str2, end, str3))))
-# }
+source('rcomcat/searchcomcat.R')
 
-dat1 = read.csv('ca_eq1.csv')
-dat2 = read.csv('ca_eq2.csv')
-dat3 = read.csv('ca_eq3.csv')
-dat4 = read.csv('ca_eq4.csv')
-dat5 = read.csv('ca_eq5.csv')
-dat6 = read.csv('ca_eq6.csv')
+stime = as.POSIXct("1970-01-01 00:00:00", format = "%Y-%m-%d %H:%M:%S", tz = "UTC")
+etime = as.POSIXct("2017-10-16 23:59:59", format = "%Y-%m-%d %H:%M:%S", tz = "UTC")
 
-full_dat = rbind(dat1,dat2,dat3,dat4,dat5,dat6)
-full_dat = full_dat %>% distinct()
+ca_dat = comcathypocsv(minmagnitude = 2,
+                    starttime = stime,
+                    endtime = etime,
+                    minlatitude = 32.495,
+                    maxlatitude = 42.183,
+                    minlongitude = -124.805,
+                    maxlongitude = -113.994,
+                    eventtype = "earthquake")
+ca_dat_store = ca_dat
 
-# write.csv(full_dat, 'ca_eq_full.csv', row.names = F)
+ok_dat = comcathypocsv(minmagnitude = 2,
+                       starttime = stime,
+                       endtime = etime,
+                       minlatitude = 33.406,
+                       maxlatitude = 37.171,
+                       minlongitude = -103.14,
+                       maxlongitude = -94.043,
+                       eventtype = "earthquake")
+ok_dat_store = ok_dat
+
+# write.csv(ca_dat, file="rawdata/earthquakes/california/ca_eq_raw.csv")
+# write.csv(ok_dat, file="rawdata/earthquakes/oklahoma/ok_eq_raw.csv")
+
 
 # The single argument to this function, pointsDF, is a data.frame in which:
 #   - column 1 contains the longitude in degrees (negative in the US)
 #   - column 2 contains the latitude in degrees
 
-latlong2state <- function(pointsDF) {
+latlong2county <- function(pointsDF) {
   # Prepare SpatialPolygons object with one SpatialPolygon
   # per state (plus DC, minus HI & AK)
-  states <- map('state', fill=TRUE, col="transparent", plot=FALSE)
-  IDs <- sapply(strsplit(states$names, ":"), function(x) x[1])
-  states_sp <- map2SpatialPolygons(states, IDs=IDs,
+  # states <- map('state', fill=TRUE, col="transparent", plot=FALSE)
+  counties <- map('county', 'california', fill=TRUE, col="transparent", plot=FALSE)
+  IDs <- sapply(strsplit(counties$names, ":"), function(x) x[1])
+  counties_sp <- map2SpatialPolygons(counties, IDs=IDs,
                                    proj4string=CRS("+proj=longlat +datum=WGS84"))
   
   # Convert pointsDF to a SpatialPoints object 
@@ -51,24 +59,27 @@ latlong2state <- function(pointsDF) {
                             proj4string=CRS("+proj=longlat +datum=WGS84"))
   
   # Use 'over' to get indices of the Polygons object containing each point 
-  indices <- over(pointsSP, states_sp)
+  indices <- over(pointsSP, counties_sp)
   
   # Return the state names of the Polygons object containing each point
-  stateNames <- sapply(states_sp@polygons, function(x) x@ID)
-  stateNames[indices]
+  countyNames <- sapply(counties_sp@polygons, function(x) x@ID)
+  countyNames[indices]
 }
 
-full_dat$state = latlong2state(full_dat[c('longitude','latitude')])
-ca_dat=subset(full_dat, full_dat$state=='california') 
-
-
 ca_dat = ca_dat %>%
-  filter(type == 'earthquake')
+        mutate(longitude = as.numeric(as.character(longitude)),
+               latitude = as.numeric(as.character(latitude)))
+ca_dat$county = latlong2county(ca_dat[ , c('longitude','latitude')])
+kern_dat=subset(ca_dat, ca_dat$county=='california,kern')
 
-ca_dat = ca_dat %>% mutate(time_year = as.numeric(substr(time, 1, 4)))
-hist(ca_dat$time_year)
+# write.csv(kern_dat, file="rawdata/earthquakes/california/kern_eq.csv")
 
-
-
-
+kern_dat = kern_dat %>% 
+            mutate(time_year = as.numeric(substr(time, 1, 4))) %>%
+            mutate(time_dec = floor(time_year/10)*10)
+hist(kern_dat$time_year)
+        
+x = get_map(location = c(lon = -119.022102, lat = 35.3733), zoom = 8)
+ggmap(x) +
+  geom_point(aes(x=longitude, y=latitude, col=as.factor(time_dec)), data = kern_dat)
 
