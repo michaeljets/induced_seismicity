02725c9049426736c34de1aa3ab77fc303b4a53a
Michael Jetsupphasuk <michaeljets@berkeley.edu>
2017-10-24 16:32:11 -0700

    stashing changes to explore.R before deletion

--- explore.R
+++ explore.R
@@ -12,12 +12,17 @@ library(rgdal)  # input/output, projections
 library(rgeos)  # geometry ops
 library(spdep)  # spatial dependence
 
+library(ggmap)
+# citation('ggmap')
+
+library(spatstat)
+
 source('rcomcat/searchcomcat.R')
 
-stime = as.POSIXct("1970-01-01 00:00:00", format = "%Y-%m-%d %H:%M:%S", tz = "UTC")
+stime = as.POSIXct("1980-01-01 00:00:00", format = "%Y-%m-%d %H:%M:%S", tz = "UTC")
 etime = as.POSIXct("2017-10-16 23:59:59", format = "%Y-%m-%d %H:%M:%S", tz = "UTC")
 
-ca_dat = comcathypocsv(minmagnitude = 2,
+ca_dat = comcathypocsv(minmagnitude = 3,
                     starttime = stime,
                     endtime = etime,
                     minlatitude = 32.495,
@@ -27,15 +32,15 @@ ca_dat = comcathypocsv(minmagnitude = 2,
                     eventtype = "earthquake")
 ca_dat_store = ca_dat
 
-ok_dat = comcathypocsv(minmagnitude = 2,
-                       starttime = stime,
-                       endtime = etime,
-                       minlatitude = 33.406,
-                       maxlatitude = 37.171,
-                       minlongitude = -103.14,
-                       maxlongitude = -94.043,
-                       eventtype = "earthquake")
-ok_dat_store = ok_dat
+# ok_dat = comcathypocsv(minmagnitude = 2,
+#                        starttime = stime,
+#                        endtime = etime,
+#                        minlatitude = 33.406,
+#                        maxlatitude = 37.171,
+#                        minlongitude = -103.14,
+#                        maxlongitude = -94.043,
+#                        eventtype = "earthquake")
+# ok_dat_store = ok_dat
 
 # write.csv(ca_dat, file="rawdata/earthquakes/california/ca_eq_raw.csv")
 # write.csv(ok_dat, file="rawdata/earthquakes/oklahoma/ok_eq_raw.csv")
@@ -66,20 +71,86 @@ latlong2county <- function(pointsDF) {
   countyNames[indices]
 }
 
+
+latlong2state <- function(pointsDF) {
+  # Prepare SpatialPolygons object with one SpatialPolygon
+  # per state (plus DC, minus HI & AK)
+  states <- map('state', fill=TRUE, col="transparent", plot=FALSE)
+  IDs <- sapply(strsplit(states$names, ":"), function(x) x[1])
+  states_sp <- map2SpatialPolygons(states, IDs=IDs,
+                                   proj4string=CRS("+proj=longlat +datum=WGS84"))
+  
+  # Convert pointsDF to a SpatialPoints object 
+  pointsSP <- SpatialPoints(pointsDF, 
+                            proj4string=CRS("+proj=longlat +datum=WGS84"))
+  
+  # Use 'over' to get indices of the Polygons object containing each point 
+  indices <- over(pointsSP, states_sp)
+  
+  # Return the state names of the Polygons object containing each point
+  stateNames <- sapply(states_sp@polygons, function(x) x@ID)
+  stateNames[indices]
+}
+
+
 ca_dat = ca_dat %>%
         mutate(longitude = as.numeric(as.character(longitude)),
-               latitude = as.numeric(as.character(latitude)))
-ca_dat$county = latlong2county(ca_dat[ , c('longitude','latitude')])
-kern_dat=subset(ca_dat, ca_dat$county=='california,kern')
+               latitude = as.numeric(as.character(latitude)),
+               time_year = as.numeric(substr(time, 1, 4)),
+               state = latlong2state(data.frame(longitude, latitude))) %>%
+        filter(state == 'california')
+
+# ca_dat$county = latlong2county(ca_dat[ , c('longitude','latitude')])
+# kern_dat=subset(ca_dat, ca_dat$county=='california,kern')
 
 # write.csv(kern_dat, file="rawdata/earthquakes/california/kern_eq.csv")
 
-kern_dat = kern_dat %>% 
-            mutate(time_year = as.numeric(substr(time, 1, 4))) %>%
-            mutate(time_dec = floor(time_year/10)*10)
-hist(kern_dat$time_year)
-        
-x = get_map(location = c(lon = -119.022102, lat = 35.3733), zoom = 8)
-ggmap(x) +
-  geom_point(aes(x=longitude, y=latitude, col=as.factor(time_dec)), data = kern_dat)
 
+# kern_dat = kern_dat %>% 
+#             mutate(time_year = as.numeric(substr(time, 1, 4))) %>%
+#             mutate(time_dec = floor(time_year/10)*10)
+# hist(kern_dat$time_year)
+#         
+# x = get_map(location = c(lon = -119.022102, lat = 35.3733), zoom = 8)
+# ggmap(x) +
+#   geom_point(aes(x=longitude, y=latitude, col=as.factor(time_dec)), data = kern_dat)
+
+
+ca_dat = ca_dat %>% 
+  mutate(time_year = as.numeric(substr(time, 1, 4))) %>%
+  mutate(time_dec = floor(time_year/10)*10)
+graphics::hist(ca_dat$time_year, breaks = 1980:2017)
+plot(x=1980:2017, y=table(ca_dat$time_year), type = 'b')
+
+# ca_map = get_map(location = 'CA', zoom = 6, source = "google")
+# ca_plot = ggmap(ca_map)
+  # geom_point(aes(x=longitude, y=latitude, col=as.factor(time_dec)), data = kern_dat)
+
+cal_sp <- SpatialPointsDataFrame(ca_dat[, c("longitude", "latitude")], 
+                                 data.frame(ID=seq(1:nrow(ca_dat))), 
+                                 proj4string=CRS("+proj=longlat +ellps=WGS84 +datum=WGS84"))
+cal_eq_points <- spTransform(cal_sp, CRS( "+proj=longlat +ellps=WGS84 +datum=WGS84" ))
+cal_eq_points_df = data.frame(cal_eq_points)
+
+california = map_data("state") %>% filter(region == 'california')
+california_sp = SpatialPointsDataFrame(california[, c("long", "lat")], 
+                                       data.frame(ID=seq(1:nrow(california))), 
+                                       proj4string=CRS("+proj=longlat +ellps=WGS84 +datum=WGS84"))
+california_points <- spTransform(california_sp, CRS( "+proj=longlat +ellps=WGS84 +datum=WGS84" ))
+california_points_df = data.frame(california_points)
+california_points_df$group = california$group
+
+
+ca_wells = readOGR(dsn = 'rawdata/wells/california/shapefiles/AllWells',
+                   layer = "AllWells_20170316")
+ca_wells_points <- spTransform(ca_wells, CRS( "+proj=longlat +ellps=WGS84 +datum=WGS84" ))
+ca_wells_df = data.frame(ca_wells_points)
+
+ca_wells_df2 = ca_wells_df %>%
+                filter(WellStatus == 'A')
+
+
+ggplot() + 
+  geom_polygon(data=california_points_df, aes(x=long,y=lat,group=group)) +
+  geom_point(data=cal_eq_points_df, aes(x=longitude,y=latitude), color = 'red')
+  # geom_point(data=ca_wells_df2, aes(x=Longitude,y=Latitude), color = 'blue')
