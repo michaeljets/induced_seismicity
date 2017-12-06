9eb84821b136ccc950d718c8eef2a6d15f04b4ec
Michael Jetsupphasuk <michaeljets@berkeley.edu>
2017-12-05 18:22:31 -0800

    updated cleaning R code

--- code/clean.R
+++ code/clean.R
@@ -27,6 +27,9 @@
 #     - See scrape/get_inj.R for injection information
 #     - Links various data frames together to gather all relevant information
 #     - Wells mapped to grid (from above)
+#     - Handles missing data
+#         + appends 0 injection for dates before first / after last
+#         + missing dates in between the first and last date have NA injection
 #     - Creates data in following formats:
 #         + long data frame (each row a combination of well, injection date)
 #         + wide data frame (each row is a unique well)
@@ -58,6 +61,101 @@ library(spatstat)
 # GLOBAL PARAMETERS -------------------------------------------------------
 
 my_crs = CRS( "+proj=longlat +ellps=WGS84 +datum=WGS84" )
+all_dates = sort(unique(wells_long$InjectionDate))
+
+fill_dates <- function(df, alldates,
+                       mindate="1980-01-01 00:00:00", 
+                       maxdate="2017-12-01 00:00:00"){
+  
+  ## Inserts the missing dates before/after the min/max date in `df`. If there are 
+  ## missing dates in between the min/max date in `df`, they are interpolated by 
+  ## taking the average of the nearest injections before and after. 
+  
+  
+  before_df = data.frame()
+  after_df = data.frame()
+  between_df = data.frame()
+  
+  before_dates = alldates[alldates < min(df$InjectionDate)]
+  after_dates = alldates[alldates > max(df$InjectionDate)]
+  
+  # fixing the "before" dates
+  if (min(df$InjectionDate) != mindate){
+    before_df = data.frame(APINumber = df$APINumber[1],
+                           InjectionDate = before_dates,
+                           WaterInjected = 0,
+                           Grid = df$Grid[1],
+                           Latitude = df$Latitude[1],
+                           Longitude = df$Longitude[1])
+  }
+  
+  # fixing the "after" dates
+  if (max(df$InjectionDate) != maxdate){
+    after_df = data.frame(APINumber = df$APINumber[1],
+                          InjectionDate = after_dates,
+                          WaterInjected = 0,
+                          Grid = df$Grid[1],
+                          Latitude = df$Latitude[1],
+                          Longitude = df$Longitude[1])
+  }
+  
+  # fixing the "between" dates
+  if (nrow(before_df)+nrow(after_df)+length(unique(df$InjectionDate)) < length(alldates)){
+    ind = which((alldates %in% c(df$InjectionDate, before_dates, after_dates)) == F)
+    between_dates = alldates[ind]
+    between_water = rep(NA, length(between_dates))
+    
+    # take the midpoint of nearest data for in-between missing data
+    for (i in 1:length(ind)){
+      
+      # find nearest water injection before
+      ind_before = ind[i]-1
+      while (alldates[ind_before] %in% between_dates){
+        ind_before = ind_before-1
+      }
+      water_before = df$WaterInjected[df$InjectionDate == alldates[ind_before]]
+      if (length(water_before) > 1){
+        water_before = ifelse(any(water_before == 0), max(water_before, na.rm=T), sum(water_before, na.rm=T))
+      }
+      
+      # find nearest water injection after
+      ind_after = ind[i]+1
+      while (alldates[ind_after] %in% between_dates){
+        ind_after = ind_after+1
+      }
+      water_after = df$WaterInjected[df$InjectionDate == alldates[ind_after]]
+      if (length(water_after) > 1){
+        water_after = ifelse(any(water_after == 0), max(water_after, na.rm=T), sum(water_after, na.rm=T))
+      }
+      
+      # average before/after water injections
+      water_inter = (water_before + water_after)/2
+      between_water[i] = water_inter
+      
+      # temp_df = df %>%
+      #             filter(InjectionDate == check_date)
+      # 
+      # if (is.na(temp_df$InjectionStatus)){
+      #   warning("Problem with injection dates")
+      # }
+      # 
+      # if (temp_df$InjectionStatus != "0"){
+      #   between_water[i] = 0
+      # }
+    }
+    
+    between_df = data.frame(APINumber = df$APINumber[1],
+                            InjectionDate = between_dates,
+                            WaterInjected = between_water,
+                            Grid = df$Grid[1],
+                            Latitude = df$Latitude[1],
+                            Longitude = df$Longitude[1])
+  }
+  
+  all_df = suppressWarnings(bind_rows(before_df, between_df, after_df, df))
+  
+  return(all_df %>% arrange(InjectionDate))
+}
 
 
 # CALIFORNIA BOUNDARIES ---------------------------------------------------
@@ -160,8 +258,7 @@ inj_wells = inj_wells %>%
               mutate(APINumber = as.numeric(APINumber)) %>%
               dplyr::select(APINumber, CountyName, DaysInjecting,
                             DistrictNumber, InjectionDate, InjectionStatus,
-                            MissingDataCode, PoolCode, PoolName,
-                            PoolWellTypeStatus, Steam.WaterInjected.BBL.,
+                            MissingDataCode, Steam.WaterInjected.BBL.,
                             SurfaceInjPressure, SystemEntryDate, WellNumber,
                             WellStatus, WellTypeCode, Year)
 
@@ -178,7 +275,8 @@ wells_long = inner_join(inj_wells, well_locs_df, by = c("APINumber")) %>%
                        Latitude = coords.x2) %>%
                 filter(Year >= 1980,
                        WellTypeCode == "WD",
-                       Longitude < -80)
+                       Longitude < -80) %>%
+                distinct()
 
 # mapping long wells to grid
 wells_long_xy = data.frame(x = wells_long$Longitude,
@@ -189,6 +287,15 @@ coordinates(wells_long_xy) = ~ x + y
 proj4string(wells_long_xy) = my_crs
 cellIDs = over(wells_long_xy, grid_sp)
 wells_long$Grid = cellIDs
+
+# handle missing injection dates
+
+all_dates = unique(wells_long$InjectionDate)
+
+wells_long = wells_long %>%
+                group_by(APINumber) %>%
+                do(fill_dates(., all_dates))
+
 
 # also create a wide data frame (i.e. each row is a unique well)
 wells_wide = wells_long %>%
@@ -223,6 +330,54 @@ wells_wide_sp = SpatialPointsDataFrame(coords = wells_wide %>%
 
 
 
+# WATER AND EARTHQUAKE BY GRID --------------------------------------------
+
+# flatten across blocks
+grid_inj = wells_wide %>%
+  dplyr::select(-APINumber, -Longitude, -Latitude) %>%
+  group_by(Grid) %>%
+  summarise_all(sum, na.rm = T)
+
+# all grid blocks with non-zero water injection
+which_no_water = (rowSums(grid_inj %>% dplyr::select(-Grid)) == 0)
+grid_inj = grid_inj %>% filter(which_no_water == F)
+
+# all grid blocks with non-zero water injection and non-zero earthquakes
+unique_grid = unique(grid_inj$Grid)
+how_many = c()
+for (i in 1:length(unique_grid)){
+  how_many[i] = sum(ca_eq$Grid %in% unique_grid[i])
+}
+
+eq_block = data.frame(Grid = unique_grid, earthquakes = how_many)
+final_blocks = sort(eq_block$Grid[eq_block$earthquakes > 0])
+
+# final data frames
+
+# convert time in ca_eq to factor with correct levels
+ca_eq = ca_eq %>%
+  mutate(time = as.character(time),
+         time = substr(time, 1, 7),
+         time = gsub('-', '.', time),
+         time = factor(time, levels = months))
+
+# generate earthquake time series
+final_eqs = ca_eq %>%
+  filter(Grid %in% final_blocks) %>%
+  arrange(Grid) %>%
+  group_by(Grid) %>%
+  dplyr::select(time) %>%
+  table() %>%
+  matrix(87, 456) %>%
+  data.frame()
+
+# final water time series
+final_water = grid_inj %>%
+  filter(Grid %in% final_blocks) %>%
+  arrange(Grid) %>%
+  dplyr::select(-Grid)
+
+
 # SAVING ------------------------------------------------------------------
 
 rm(inj_wells)
@@ -231,3 +386,116 @@ rm(well_locs)
 rm(well_locs_df)
 save.image(file = "clean_data.RData")
 save.image()
+
+
+# Writing -----------------------------------------------------------------
+
+# write files
+write.csv(final_water, file = "data/final_water.csv", row.names = F)
+write.csv(final_eqs, file = "data/final_eqs.csv", row.names = F)
+write.table(final_blocks, file = "data/final_blocks.txt", row.names = F, col.names = F)
+
+
+# ARCHIVED CODE -----------------------------------------------------------
+
+# wells_long2 = wells_long %>%
+#   group_by(APINumber) %>%
+#   mutate(Latitude = Latitude[is.na(Latitude)==F][1],
+#          Longitude = Longitude[is.na(Longitude)==F][1])
+
+
+# compute_d <- function(x,y){
+#   dot = sum(x*y)
+#   n = length(x)
+#   result = n*(n+1)*(2*n+1)/3 - 2*dot
+#   return(result)
+# }
+# 
+# # Do not "re-rank" at each lag; i.e. do not throw away information
+# # Which lag tends to have the highest rank correlation?
+# set.seed(12879273)
+# B = 100000
+# max_lag = c()
+# a = 1:450
+# for (i in 1:B){
+#   b = sample(1:450, 450, replace=F)
+#   ccfs = ccf(a, b, plot = F, lag.max = 450)
+#   cors = as.numeric(ccfs$acf)
+#   lags = as.numeric(ccfs$lag)
+#   which_maxs = which(cors == max(cors))
+#   which_max = ifelse(length(which_maxs)==1, which_maxs, sample(which_maxs, 1))
+#   max_lag[i] = lags[which_max]
+# }
+# max_lag2 = factor(max_lag, levels = -449:449)
+# plot(table(max_lag2))
+# 
+# # "Re-rank" at each lag
+# # Which lag tends to have the highest rank correlation?
+# B = 1000
+# max_lag2 = c()
+# a = 1:200
+# for (i in 1:B){
+#   b = sample(1:200, 200, replace=F)
+#   ccfs = c()
+#   for (l in 0:198){
+#     a2 = a[1:(200-l)]
+#     b2 = b[(l+1):200]
+#     # a2 = rank(a2) # re-rank
+#     # b2 = rank(b2) # re-rank
+#     ccfs[l+1] = cor(a2,b2)
+#   }
+#   max_lag2[i] = which(ccfs == max(ccfs)[1]) - 1
+# }
+# max_lag2 = factor(max_lag2, levels = 0:199)
+# plot(table(max_lag2))
+# 
+#=================================================#
+# # Don't use all data; use let's say 400
+# B = 10000
+# max_lag = c()
+# a = 1:150
+# b = 1:200
+# for (i in 1:B){
+#   ccfs = c()
+#   b = sample(b, 200, replace=F)
+#   for (l in 0:12){
+#     b2 = b[(l+1):(150+l)]
+#     b2 = rank(b2) # re-rank
+#     ccfs[l+1] = cor(a,b2)
+#   }
+#   max_lag[i] = which(ccfs == max(ccfs))[1] - 1
+# }
+# max_lag2 = factor(max_lag, levels = 0:12)
+# plot(table(max_lag2))
+
+# diff_months <- function(end_date, start_date) {
+#   ed <- as.POSIXlt(end_date)
+#   sd <- as.POSIXlt(start_date)
+#   return(12 * (ed$year - sd$year) + (ed$mon - sd$mon))
+# }
+# 
+# 
+# unique_id = paste0(wells_long$APINumber, '/', wells_long$InjectionDate)
+# length(unique_id)
+# length(unique(unique_id))
+# which_wrong = names(table(unique_id)[table(unique_id) > 1])
+# wells_long$unique_id = unique_id
+# 
+# View(wells_long %>% 
+#        filter(unique_id %in% which_wrong) %>% 
+#        arrange(APINumber, InjectionDate) %>% 
+#        select(APINumber, InjectionDate, WaterInjected))
+# 
+# x=wells_long %>%
+#   group_by(APINumber) %>%
+#   summarize(length(InjectionDate) - diff_months(max(InjectionDate), min(InjectionDate)) - 1) %>%
+#   as.data.frame()
+# colnames(x) = c('APINumber', 'months')
+# 
+# x=wells_long %>%
+#   group_by(APINumber) %>%
+#   mutate(dup = ifelse(length(InjectionDate) == length(unique(InjectionDate)), 'duplicates', 'ok')) %>%
+#   select(APINumber, dup) %>%
+#   as.data.frame() %>%
+#   distinct()
+# table(x$dup)
