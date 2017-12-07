658bbe9b02aa89ba9772f23fcde851106aaad687
frankmei33 <jimei@berkeley.edu>
2017-12-06 22:51:20 -0800

    tested uncorrelated data

--- code/simulate.py
+++ code/simulate.py
@@ -23,6 +23,7 @@ from get_eq import get_eq
 import csv
 import random
 
+
 ##############################################################################
 
 ## GET TIME TO RUN SCRIPT
@@ -33,7 +34,7 @@ starttime = datetime.now()
 ## LOAD IN REAL WATER DATA
 
 # `water` is a list of lists with each internal list representing water
-# injections from 1980-2017 (length 456) for a distinct block (87 blocks
+# injections from 1980-2017 (length 450) for a distinct block (87 blocks
 # in total). 
 
 water = []
@@ -56,12 +57,21 @@ use_water = np.array(water[random_water])
 # generate earthquake data
 # np.random.seed(157157)
 eqs = get_eq(use_water)
+dates = np.arange('1980-01', '2017-07', dtype='datetime64[M]')
 
 # plot water and earthquake data
-plt.plot(use_water)
+plt.plot(dates, use_water)
+plt.xticks(dates[::60])
+plt.xlabel('dates')
+plt.ylabel('bbl')
+plt.savefig('water_injections.png')
 plt.show()
 
-plt.plot(eqs)
+plt.plot(dates, eqs)
+plt.xticks(dates[::60])
+plt.xlabel('dates')
+plt.ylabel('counts')
+plt.savefig('earthquakes.png')
 plt.show()
 
 # rank data
@@ -69,8 +79,29 @@ eqs_rank = rankdata(eqs)
 water_rank = rankdata(use_water)
 
 # run simulation
-pval = sim.corr_test(water_rank, eqs_rank, norm = 2, plot = True)
+pval = sim.corr_test(water_rank, eqs_rank, norm = 2, plot = True, filename='corr_p.png')
 print("P-value: ", pval)
+
+##############################################################################
+
+# ## TEST UNCORRELATED DATA
+
+#eqs = np.tile(np.array([0]*56+[10]),8)
+#eqs = np.array([np.exp(np.random.randn(1) + (4 if i % 50 == 0 else 0)) for i in range(450)])
+
+# rank data
+#eqs_rank = rankdata(eqs)
+
+#plt.plot(dates, eqs)
+#plt.xticks(dates[::60])
+#plt.xlabel('dates')
+#plt.ylabel('counts')
+#plt.savefig('earthquakes_uncorr.png')
+#plt.show()
+
+# run simulation
+#pval = sim.corr_test(water_rank, eqs_rank, norm = 2, plot = True, filename='uncorr_p.png')
+#print("P-value: ", pval)
 
 
 ##############################################################################
